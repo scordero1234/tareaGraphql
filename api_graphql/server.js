6da78047e1c28a46d0876b589b7e9404cffe18bf
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-10-19 21:14:23 -0500

    base

--- api_graphql/server.js
+++ api_graphql/server.js
@@ -40,26 +40,6 @@ let root = {
         return newCliente.save();
     },
 };
-/*
-let clientes = []
-let counter = 1
-
-let root = {
-    clientes: () => { return clientes },
-    cliente: (data) => {
-        for (let i=0; i<clientes.length; i++) {
-            if (clientes[i].id == data.id) {
-                return clientes[i]
-            }
-        }
-    },
-    addCliente: (data) => {
-        let objeto = {'id':counter, 'nombre':data.nombre, 'telefono': data.telefono}
-        clientes.push( objeto )
-        counter++
-        return objeto
-    }
-}*/
 
 const config = require('./config') 
 const db = require('./db')
